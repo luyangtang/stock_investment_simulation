9a0c5b0c4eb33e70f798d1d9b74faf06e37b60e7
luyangtang <kojika.tang@gmail.com>
2020-04-22 17:46:31 +0100

    initial commit

---
+++ dataFetch/__init__.py
@@ -0,0 +1,88 @@
+#%%
+# Import yfinance
+import yfinance as yf  
+import pandas as pd
+import warnings
+
+
+class StockData(object):
+
+    def __init__(self, symbol, start = '2000-01-01', end = '2020-04-20', loadFromApi = True, exportDataTo = None, dataPath = None):
+
+        self.symbol = symbol
+        self.start = start
+        self.end = end
+        self.data = pd.DataFrame()
+        
+        self.flag = 0 # for test cases
+
+
+        # a load from api
+        if loadFromApi:
+            
+            try:
+            
+                # Get the data for the stock symbol by specifying the stock ticker, start date, and end date
+                self.data = yf.download(self.symbol, self.start, self.end)
+                self.flag = 1
+
+                
+                # export the data if exportData = True
+                if exportDataTo:
+                    self.data.to_csv(exportDataTo)
+
+                    self.flag = 2
+
+            
+            except Exception as err:
+                print(err)
+        
+
+        # else get data from dataPath specified
+        elif dataPath:
+
+            try:
+
+                # read data into df from the specified path
+                self.data = pd.read_csv(dataPath)
+                self.flag = 3
+
+            
+            except Exception as err:
+                print(err)
+
+
+        # raise an error if dataPath not provided
+        else:
+            
+            raise(Exception('dataPath must be provided if not loading from api'))
+
+
+        # show warning if no data created
+        if self.data.empty:
+            warnings.warn('No data loaded')
+
+    
+    def getData(self):
+
+        return self.data
+
+
+
+#%%
+data = StockData(
+    symbol = 'IVV'
+).getData()
+
+# # %%
+# # Plot the close prices
+# import matplotlib.pyplot as plt
+# data.Close.plot()
+# plt.show()
+# # %%
+
+
+# data
+
+# # %%
+
---
+++ tests/unit_tests.py
@@ -0,0 +1,90 @@
+
+import unittest
+import os
+
+# DataFetch
+
+#%% stock
+
+target = __import__("dataFetch")
+StockData = target.StockData
+
+class TestStockDataInit(unittest.TestCase):
+
+    def test_load_from_api(self):
+        """
+        Test that it can load data from api
+        """
+        symbol = 'IVV'
+        start = '2019-01-01'
+        end = '2020-01-01'
+        
+        stockData = StockData(
+            symbol = symbol,
+            start = start,
+            end = end,
+            loadFromApi = True,
+            exportDataTo = False,
+            dataPath = None
+        )
+
+        self.assertEqual(stockData.flag, 1)
+        self.assertFalse(stockData.data.empty)
+        # can also check if min = start, max = end
+
+
+    
+    def test_load_from_api_with_export(self):
+        """
+        Test that it can load data from api
+        """
+        symbol = 'IVV'
+        start = '2019-01-01'
+        end = '2020-01-01'
+        exportDataTo = './test_data_export.csv'
+        
+        stockData = StockData(
+            symbol = symbol,
+            start = start,
+            end = end,
+            loadFromApi = True,
+            exportDataTo = exportDataTo,
+            dataPath = None
+        )
+
+        self.assertEqual(stockData.flag, 2)
+        self.assertFalse(stockData.data.empty)
+        self.assertTrue(os.path.isfile(exportDataTo))
+        # remove the file
+        os.remove(exportDataTo)
+        # can also check if min = start, max = end
+
+
+    
+    def test_load_from_file(self):
+        """
+        Test that it can load data from file
+        """
+        symbol = 'IVV'
+        start = '2019-01-01'
+        end = '2020-01-01'
+        dataPath = './tests/test_stock_data'
+
+        stockData = StockData(
+            symbol = symbol,
+            start = start,
+            end = end,
+            loadFromApi = False,
+            exportDataTo = None,
+            dataPath = dataPath
+        )
+
+        self.assertEqual(stockData.flag, 3)
+        self.assertFalse(stockData.data.empty)
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
+# %%
